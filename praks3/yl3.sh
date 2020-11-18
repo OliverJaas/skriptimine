359bc7a55e7e524c7103f92bb6738e9341cd4e35
OliverJaas <72203167+OliverJaas@users.noreply.github.com>
2020-11-18 14:16:05 +0200

    Update yl3.sh
    Arvutusülesande funktsioonide lisamine ja arvutamine

--- praks3/yl3.sh
+++ praks3/yl3.sh
@@ -1,1 +1,12 @@
-tee n++
+#!/bin/bash
+#arvutusülesanne EAP oma
+
+echo "Palun sisestage ainepunktide arv: "
+read eap
+echo "Palun sisestage nädalate arv: "
+read ndl
+
+aeg=`echo "scale=1;$eap*26/$ndl" | bc` 
+echo $aeg
+aeg_ymarda="`echo "($aeg+0.9)/1" | bc`"
+echo $aeg_ymarda
