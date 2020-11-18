c1826eb7dcf1fae184ce706621d3881a36b0a4f4
OliverJaas <72203167+OliverJaas@users.noreply.github.com>
2020-11-18 13:22:42 +0200

    Update yl2.sh
    Lisasin hetkese aja juurde

--- praks2/yl2.sh
+++ praks2/yl2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Annab kasutajale t2nase kuup2eva ja ning selle kuu kalendri
-date
+now=$(date +"%T")
+echo "Hetkene kellaaeg: $now"
 echo "Tere $USER"
 echo "Siin on t2nane kuup2ev"
 cal
