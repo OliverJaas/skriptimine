6f7fb4b433552cda37fca2b2dbdb05a553875193
OliverJaas <72203167+OliverJaas@users.noreply.github.com>
2020-11-18 14:28:41 +0200

    Create yl4.sh
    Ekskursiooniabi

---
+++ praks3/yl4.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+#Bussiekskursioonide korraldajale abi, et palju on reisijaid grupis ja palju bussi mahub
+#ning palju maha jääb reisijaid, sest bussikohti pole
+
+
+grupis=reisijad
+kohad=bussikohad
+
+echo "Mitu reisijat on grupis: "
+read reisijad
+echo "Mitu kohta on ühes bussis: "
+read bussikohad
+
+taisbusse=$(( bussikohad / reisijad ))
+buss_ymarda="`echo "($taisbusse+0.9)/1" | bc`" 
+mahajaanud=$((reisijad - bussikohad))
+
+echo "Täis busse on $buss_ymarda ja maha on jäänud $mahajaanud" 
