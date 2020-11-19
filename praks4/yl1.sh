4c5ca1994b2f2e670e8474e3f42c1016a2b8142b
Oliver Jaas <oliver.jaas@khk.ee>
2020-11-19 16:52:13 +0200

    fikseerimine lihtsamaks koodiks

--- praks4/yl1.sh
+++ praks4/yl1.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #Annab kasutajale teada kas sisestatud arv on paaris või paaritu
 
-read -p "Sisesta number et teada kas tegu on paaritu või paaris arvuga: " number
+echo  "Sisesta number et teada kas tegu on paaritu või paaris arvuga: "
+read number
 if [ $((number%2)) -eq 0 ]
 then
   echo "Tegu on paarisarvuga."
